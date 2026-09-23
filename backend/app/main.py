cd4d868cd92349415557661362d2187673a56da9
gungun <mahajangungun97@gmail.com>
2026-09-23 21:22:18 +0530

    Fix production frontend CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,12 +27,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
+     allow_origins=[
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://sql-copilot-fkh2vw7w3-gunguns-projects-3da18e36.vercel.app"
+    "https://sql-copilot-tau.vercel.app"
 ],
-     
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
